4bace03abebb43c00512f771e9cd4ddd9123adcb
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-04-30 16:38:08 +0200

    derniere save exo5js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,30 @@
-let phrase = 'Bonjour tout le monde ';
+//1
+let phrase = 'Bonjour tout le monde    ';
+//2
 console.log(phrase.length);
-console.log(phrase.trimEnd());
+//3
+console.log(phrase.trimEnd())
+//4
+phrase = phrase.trimEnd();
 console.log(phrase.length);
+//5
 console.log(phrase.charAt(20));
-console.log(phrase.substr(8,20));
-console.log(phrase.replace("Bonjour","Bonjour"));
-console.log(phrase.substr(0,7));
+//console.log(phrase.charAt(phrase.lenght-1)); meilleure solution!
+
+//6
+phrase = phrase.substr(8);
+console.log(phrase);
+//7
+phrase = phrase.replace("", "Bonjour ");
+console.log(phrase);
+//8
+phrase = phrase.substr(0,7);
+console.log(phrase);
+//9
 let phrase2 = "J'aime les numbers"
-console.log(phrase2.replace("numbers","string"))
+phrase2 = phrase2.replace("numbers","string")
+console.log(phrase2)
+//10
 var min=1; 
 var max=1000;  
 var random = Math.floor(Math.random() * (max - min)) + min; 
